0b7356be5e32a5b69201a2403cf5012949601041
komalkadam-dc <154054016+komalkadam-dc@users.noreply.github.com>
2025-06-16 15:45:58 +0530

    Removed the code, added cost part in dollars and updated dockerfile

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from agent_server import create_chat_app
 from services.llm import BedrockAnthropicLLM
 from agents.echo_agent import EchoAgent
 from agents.llm_passthrough_agent import LLMPassthroughAgent
-from agents.cost_optimization_agent import CostOptimizationAgent
-from agents.cost_cmd_agent import CostOptimizationCommandAgent
+from agents.cost_optimization_agent import CostOptimizationCommandAgent
 from agents.cmd_agent import CommandAgent
 from agents.boilerplate_agent import BoilerplateAgent
 import dotenv
